c0c28dc1d31c75e307095ec5f0d008b8ccd55437
flutter <alkadafifirnawan1@gmail.com>
2026-07-22 15:19:26 +0700

    feat: vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
 
-require __DIR__ . "/../public/index.php";
+// Pindah ke direktori public
+chdir(__DIR__ . '/../public');
+
+// Require index.php Laravel
+require_once __DIR__ . '/../public/index.php';
--- vercel.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$_SERVER['SCRIPT_NAME'] = '/index.php';
-$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/public/index.php';
-
-chdir(__DIR__ . '/public');
-
-require __DIR__ . '/public/index.php';
